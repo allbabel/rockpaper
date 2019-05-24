b53c5bd7b64458f0df6e8e67860981170b7f36ad
Andy Bell <allbabel@gmail.com>
2019-05-24 11:45:15 +0200

    Running with wagers

--- contracts/RockPaper.sol
+++ contracts/RockPaper.sol
@@ -2,8 +2,6 @@ pragma solidity 0.5.0;
 import "./Running.sol";
 import "./SafeMath.sol";
 
-pragma solidity 0.5.0;
-
 contract RockPaper is Running
 {
     using SafeMath for uint256;
@@ -11,7 +9,7 @@ contract RockPaper is Running
     mapping(address => Game) public games;
 
     enum Guess {NONE, ROCK, PAPER, SCISSORS}
-    Guess[3][3] matrix =
+    Guess[3][3] rules =
     [
         [Guess.NONE, Guess.PAPER, Guess.ROCK],
         [Guess.PAPER, Guess.NONE, Guess.SCISSORS],
@@ -20,16 +18,32 @@ contract RockPaper is Running
 
     struct Game
     {
-        uint wager;
         address player1;
         bytes32 guess1;
+        uint wager1;
         address player2;
         uint8 guess2;
+        uint wager2;
     }
 
     event LogGameCreated(address indexed player1, uint wager, bytes32 guess1);
-    event LogGameCompleted(address indexed player1, address indexed player2, uint wager, bytes32 guess1, uint8 guess2);
-    event LogGameSettled(address indexed player1, address indexed player2, address indexed winner, uint wager, bytes32 guess1, uint8 guess2);
+
+    event LogGameCompleted( address indexed player1,
+                            address indexed player2,
+                            uint wager1,
+                            uint wager2,
+                            bytes32 guess1,
+                            uint8 guess2);
+
+    event LogGameSettled(   address indexed player1,
+                            address indexed player2,
+                            address indexed winner,
+                            uint wager1,
+                            uint wager2,
+                            bytes32 guess1,
+                            uint8 guess2);
+
+    event LogGameDrawn(address indexed player1, address indexed player2);
 
     constructor()
         public
@@ -56,9 +70,10 @@ contract RockPaper is Running
         require(games[msg.sender].player1 == address(0x0), 'Game already exists');
         require(uint(guess) > 0, 'Invalid guess');
 
-        games[msg.sender] = Game( { wager: msg.value,
+        games[msg.sender] = Game( { wager1: msg.value,
                                     player1: msg.sender,
                                     guess1: guess,
+                                    wager2: 0,
                                     player2: address(0x0),
                                     guess2: 0} );
 
@@ -73,12 +88,14 @@ contract RockPaper is Running
     {
         Game storage g = games[game];
         require(g.player1 != address(0x0), "Game doesn't exists");
+        require(g.player1 != msg.sender, 'This is your game');
         require(guess > uint8(Guess.NONE) && guess <= uint8(Guess.SCISSORS), 'Bad guess');
 
         g.player2 = msg.sender;
         g.guess2 = guess;
+        g.wager2 = msg.value;
 
-        emit LogGameCompleted(g.player1, g.player2, g.wager, g.guess1, g.guess2);
+        emit LogGameCompleted(g.player1, g.player2, g.wager1, g.wager2, g.guess1, g.guess2);
     }
 
     // The game owner now has to settle the game, in other words reveals their guess to the network
@@ -94,10 +111,23 @@ contract RockPaper is Running
         require(g.guess1 == encodeGuess(guess, seed), 'Invalid guess');
 
         address winner = determineWinner(g, guess);
-        winnings[winner] = winnings[winner].add(g.wager);
-        delete games[msg.sender];
+        if (winner == address(0x0))
+        {
+            // return wager to player1 and player2
+            winnings[g.player1] = winnings[g.player1].add(g.wager1);
+            winnings[g.player2] = winnings[g.player2].add(g.wager2);
 
-        emit LogGameSettled(g.player1, g.player2, winner, g.wager, g.guess1, g.guess2);
+            emit LogGameDrawn(g.player1, g.player2);
+        }
+        else
+        {
+            // Winner takes all
+            winnings[winner] = winnings[winner].add(g.wager1 + g.wager2);
+            emit LogGameSettled(g.player1, g.player2, winner, g.wager1, g.wager2, g.guess1, g.guess2);
+        }
+
+        // End game
+        delete games[msg.sender];
     }
 
     function determineWinner(Game storage g, uint8 guess)
@@ -107,7 +137,7 @@ contract RockPaper is Running
     {
         if (guess != g.guess2)
         {
-            if (matrix[guess][g.guess2] == Guess(guess))
+            if (rules[guess][g.guess2] == Guess(guess))
             {
                 return g.player1;
             }
@@ -115,6 +145,7 @@ contract RockPaper is Running
             return g.player2;
         }
 
+        // A draw
         return address(0x0);
     }
 
