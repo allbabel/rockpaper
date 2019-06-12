a3ba4cc10fe2c8e26a684d16654748f006b79b42
Andy Bell <andy.bell@displaynote.com>
2019-06-12 12:51:39 +0200

    Allow a player to bet a combination of funds sent to the contract and/or winnings they may have already

--- contracts/RockPaper.sol
+++ contracts/RockPaper.sol
@@ -19,19 +19,24 @@ contract RockPaper is Running
     struct Game
     {
         address player1;
-        uint wager;
         address player2;
+
+        uint wager; // This can be split between value sent to contract and winnings from both players
+        mapping(address => uint) wagerFromWinnings;
+
         Guess plainGuess2;
     }
 
     event LogGameCreated(   address indexed player1,
                             address indexed player2,
                             uint wager,
+                            uint player1WagerFromWinnings,
                             bytes32 gameId);
 
     event LogGameCompleted( address indexed player1,
                             address indexed player2,
                             uint wager,
+                            uint player2WagerFromWinnings,
                             bytes32 gameId,
                             Guess plainGuess2);
 
@@ -39,6 +44,7 @@ contract RockPaper is Running
                             address indexed player2,
                             address indexed winner,
                             uint wager,
+                            uint winningsLost,
                             bytes32 gameId,
                             Guess plainGuess2);
 
@@ -76,39 +82,43 @@ contract RockPaper is Running
     // A player starts a game on the chain, they send their guess encoded using encodeGuess
     // They would send value, which is optional, this would create a game
     // msg.sender is set as player1, passes hashed guess, sets wager and player2
-
-    function startGame(bytes32 gameId, address player2)
+    // If the player has funds in the contract then they can be used
+    function startGame(bytes32 gameId, address player2, uint player1WagerFromWinnings)
         public
         payable
     {
         // Check parameters
         require(isValidBytes32(gameId), 'Invalid guess');
         require(isValidAddress(player2), 'We need player2');
-
+        // User sends no payment and has insufficient funds in their winnings slot
+        require(winnings[msg.sender] >= player1WagerFromWinnings, 'Insufficient balance');
         // Player2 cannot be Player1
         require(player2 != msg.sender, 'Player1 cannot be Player2');
         // Look up game from storage, if we have player1 set the game exists
         require(!isValidAddress(games[gameId].player1), 'Game already exists');
 
         // Create game
-        games[gameId] = Game( { wager: msg.value,
-                                    player1: msg.sender,
-                                    player2: player2,
-                                    plainGuess2: Guess.NONE} );
+        games[gameId] = Game( { wager: msg.value + player1WagerFromWinnings,
+                                player1: msg.sender,
+                                player2: player2,
+                                plainGuess2: Guess.NONE} );
 
+        games[gameId].wagerFromWinnings[msg.sender] = player1WagerFromWinnings;
         // Emit event of game created
-        emit LogGameCreated(msg.sender, player2, msg.value, gameId);
+        emit LogGameCreated(msg.sender, player2, msg.value, player1WagerFromWinnings, gameId);
     }
 
     // A second player would join a game, they send a guess which isn't encoded
     // They would also send value, which should match the wager, this would close the game
-    function joinGame(bytes32 gameId, Guess plainGuess)
+    function joinGame(bytes32 gameId, Guess plainGuess, uint player2WagerFromWinnings)
         public
         payable
         onlyValidGuess(plainGuess)
     {
         // Check parameters
         require(isValidBytes32(gameId), 'Invalid game');
+        // User sends no payment and has insufficient funds in their winnings slot
+        require(winnings[msg.sender] >= player2WagerFromWinnings, 'Insufficient balance');
 
         Game storage g = games[gameId];
         // Only player2 can call this
@@ -116,15 +126,16 @@ contract RockPaper is Running
         // Is this game valid?
         require(g.plainGuess2 == Guess.NONE, 'Player2 already submitted their move for this game');
         // Check wager matches
-        require(g.wager == msg.value, 'Wager does not match');
+        require(g.wager == msg.value + player2WagerFromWinnings, 'Wager does not match');
         // Is this game complete?
-        require(!isValidGame(g), 'Game is complete');
+        require(g.plainGuess2 == Guess.NONE, 'Player2 already submitted their move for this game');
 
         // Store plain guess for player2
         g.plainGuess2 = plainGuess;
+        g.wagerFromWinnings[msg.sender] = player2WagerFromWinnings;
 
         // Emit event of game being completed and now ready to be settled by player1
-        emit LogGameCompleted(g.player1, g.player2, g.wager, gameId, g.plainGuess2);
+        emit LogGameCompleted(g.player1, g.player2, g.wager, player2WagerFromWinnings, gameId, g.plainGuess2);
     }
     // The game owner now has to settle the game, in other words reveals their guess to the network
     // This is then checked with what they had guessed in creating the game
@@ -146,13 +157,16 @@ contract RockPaper is Running
         require(gameId == createGameId(plainGuess, seed), 'Invalid guess');
 
         // Calculate winner
-        address winner = determineWinner(g, plainGuess);
+        (address winner, address loser) = determineWinner(g, plainGuess);
 
         if (isValidAddress(winner))
         {
             // Winner takes all
             winnings[winner] = winnings[winner].add(g.wager).add(g.wager);
-            emit LogGameSettled(g.player1, g.player2, winner, g.wager, gameId, g.plainGuess2);
+            // Loser
+            winnings[loser] = winnings[loser].sub(g.wagerFromWinnings[loser]);
+
+            emit LogGameSettled(g.player1, g.player2, winner, g.wager, g.wagerFromWinnings[loser], gameId, g.plainGuess2);
         }
         else
         {
@@ -185,20 +199,20 @@ contract RockPaper is Running
     function determineWinner(Game storage g, Guess guess)
         private
         view
-        returns (address)
+        returns (address winner, address loser)
     {
         if (guess != g.plainGuess2)
         {
             if (rules[uint(guess) - 1][uint(g.plainGuess2) - 1] == Guess(guess))
             {
-                return g.player1;
+                return (g.player1, g.player2);
             }
 
-            return g.player2;
+            return (g.player2, g.player1);
         }
 
         // A draw
-        return address(0x0);
+        return (address(0x0), address(0x0));
     }
 
     function isValidAddress(address addr)
